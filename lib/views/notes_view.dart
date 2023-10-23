116dc5e382448b01c7d11512c0bb349f823df06a
abdelrahmanm27 <bedo27boda93@gmail.com>
2023-10-23 15:04:55 +0300

    floating action button & create modal bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,12 +1,26 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/add_notes_bottom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return AddNoteBottomSheet();
+            },
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
       body: NotesViewBody(),
     );
   }
--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -2,7 +2,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:notes_app/widgets/custom_app_bar.dart';
-import 'package:notes_app/widgets/note_item.dart';
 import 'package:notes_app/widgets/notes_list_view.dart';
 
 class NotesViewBody extends StatelessWidget {
@@ -16,9 +15,6 @@ class NotesViewBody extends StatelessWidget {
             height: 40,
           ),
           CustomAppBar(),
-          SizedBox(
-            height: 16,
-          ),
           Expanded(
             child: NotesListView(),
           ),
