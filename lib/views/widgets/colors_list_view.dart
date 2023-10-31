231861b1c17daeb274822040a818c32bc693aa8b
abdelrahmanm27 <bedo27boda93@gmail.com>
2023-10-31 18:04:23 +0200

    colors list view palette

--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
 
-class ColorsListView extends StatelessWidget {
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key, required this.isActive, required this.color});
+  final bool isActive;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ? CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.white,
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: 26,
+            ),
+          )
+        : CircleAvatar(
+            backgroundColor: color,
+            radius: 30,
+          );
+  }
+}
+
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xffac3931),
+    Color(0xffe5d352),
+    Color(0xffd9e76c),
+    Color(0xff537d8d),
+    Color(0xff482c3d),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -9,26 +46,26 @@ class ColorsListView extends StatelessWidget {
       height: 60,
       child: ListView.separated(
         separatorBuilder: (context, index) {
-          return const SizedBox(width: 5,);
+          return const SizedBox(
+            width: 5,
+          );
         },
-        itemCount: 5,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const ColorItem();
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                currentIndex = index;
+              });
+            },
+            child: ColorItem(
+              color: colors[index],
+              isActive: currentIndex == index,
+            ),
+          );
         },
       ),
-    );
-  }
-}
-
-class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const CircleAvatar(
-      backgroundColor: Colors.blue,
-      radius: 30,
     );
   }
 }
